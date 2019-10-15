ab0c205510bd093c7b448e896d697f49efb2aba0
李彦风 <liyanfeng@liyanfengdeMacBook-Pro.local>
2019-10-15 15:00:47 +0800

    first init

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,54 @@
+module.exports = {
+  title: 'blog',
+  base: '/blog/',
+  description: 'record my learning road',
+  // webpack配置
+  configureWebpack: {
+    resolve: {
+      alias: {
+        '@alias': './public'
+      }
+    }
+  },
+  shouldPrefetch: () => false,
+  // 主题配置
+  themeConfig: {
+    // 假定是 GitHub. 同时也可以是一个完整的 GitLab URL
+    repo: 'muzi131313/blog',
+    // 自定义仓库链接文字。默认从 `themeConfig.repo` 中自动推断为
+    // "GitHub"/"GitLab"/"Bitbucket" 其中之一，或是 "Source"。
+    repoLabel: 'Github',
+    // 自动生成侧边栏
+    // sidebar: 'auto',
+    // ==== 可选配置 ===
+
+    // 假如你的文档仓库和项目本身不在一个仓库：
+    // docsRepo: 'vuejs/vuepress',
+    // 假如文档不是放在仓库的根目录下：
+    docsDir: 'docs',
+    // 假如文档放在一个特定的分支下：
+    // docsBranch: 'sourcecode',
+    // 默认是 false, 设置为 true 来启用
+    editLinks: true,
+    // 默认为 "Edit this page"
+    editLinkText: '帮Roast Wind改善此页面！',
+    // 菜单栏
+    nav: [
+      { text: '概述', link: '/' },
+      { text: '前端基础', link: '/basic/' },
+      { text: 'GitHub', link: 'https://github.com/muzi131313/blog' }
+    ],
+    // 侧边栏
+    sidebar: {
+      '/basic/': [
+        {
+          title: '前端基础',
+          collapsable: true,
+          children: [
+            '/basic/front/front-1.md'
+          ]
+        }
+      ]
+    }
+  }
+}
---
+++ docs/README.md
@@ -0,0 +1,14 @@
+---
+home: true
+heroImage: img/roastwind.png
+actionText: go, go →
+actionLink: /basic/
+features:
+- title: 前端基础
+  details: 深挖前端基础, 补全自己的前端体系, 增强自己的前端技能, 做一个合格的前端开发者
+- title: Vue/React学习
+  details: 深入学习Vue, 合理的使用Vue, 总结自己的最佳实践, 为Vue的生态增砖添瓦; 享受纯js编程的乐趣
+- title: 程序编程
+  details: 设计模式学习, 操作系统, 数据算法, 开源项目, 各种小插件, 享受程序员的快乐
+footer: MIT Licensed | Copyright © 2019-present Roast Wind
+---
