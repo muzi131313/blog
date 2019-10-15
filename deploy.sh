51040a46bc5cead08b634218f667398b16ca095e
roastwind <muzi131313@gmail.com>
2019-10-15 15:24:58 +0800

    init: update git push to gh-pages process

--- deploy.sh
+++ deploy.sh
@@ -9,21 +9,22 @@ npm run docs:build
 cp README.md docs/.vuepress/dist/
 
 # 进入生成的文件夹
-# cd docs/.vuepress/dist
+cd docs/.vuepress/dist
 
 # 如果是发布到自定义域名
 # echo 'www.example.com' > CNAME
 
-# git init
-# git add -A
-# git commit -m 'deploy'
-# git remote add origin https://github.com/muzi131313/blog.git
+git init
+git add -A
+git commit -m 'deploy'
+git remote add origin https://github.com/muzi131313/blog.git
 # git push origin master -f
 
 # 如果发布到 https://<USERNAME>.github.io
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
-node deploy.js
+git push -f https://github.com/muzi131313/blog.git master:gh-pages
+# node deploy.js
 
-# cd -
+cd -
